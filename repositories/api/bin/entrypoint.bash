636a60ca8a960ce7901d0a04737d3606da2354f0
damlys <damian.lysiak@gmail.com>
2018-03-01 19:19:01 +0100

    replace links with depends_on in docker-compose files

--- repositories/api/bin/entrypoint.bash
+++ repositories/api/bin/entrypoint.bash
@@ -5,21 +5,15 @@ set -e
 case $1 in
     "--start")
         envsubst '$$APP_HTTP_AUTH_BASIC' < /etc/nginx/sites-available/default.template > /etc/nginx/sites-available/default
-        nginx
+        nginx -g 'daemon on;'
 
-        php-fpm
+        php-fpm --nodaemonize
     ;;
 
     "--start-cron")
         (printenv | sed 's/^\(.*\)\=\(.*\)$/export \1\=\2/g' | grep -E '^export APP_') > /etc/cron-envvars
-
         crontab /etc/crontab
-
         cron -f
-    ;;
-
-    "--install-xdebug")
-        pecl install xdebug && docker-php-ext-enable xdebug
     ;;
 
     *)
